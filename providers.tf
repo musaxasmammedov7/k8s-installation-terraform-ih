b693596583f7b3b2a8957c783d110a2f4f13c535
musaxasmammedov7 <musaxasmammedov7@icloud.com>
2026-05-13 11:18:55 +0400

    mmm

--- providers.tf
+++ providers.tf
@@ -11,7 +11,9 @@ provider "azurerm" {
   # Configuration options
 
   features {
-
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
   }
 
   subscription_id = "5a775aa2-0a21-483c-9818-460896054c2e"
